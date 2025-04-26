9f4c6e29c5ef741bc78e2e67b869cff16db196e7
Leo Chan <leo.chan@tokuindustry.com>
2025-04-26 14:45:50 -0600

    add bolt iamge

--- src/bolt.ts
+++ src/bolt.ts
@@ -1,6 +1,7 @@
 import * as ex from 'excalibur';
 import { Level } from './level';
 import { Poo } from './poo';
+import { Resources } from './resources';
 
 export class Bolt extends ex.Actor {
   constructor(
@@ -15,6 +16,10 @@ export class Bolt extends ex.Actor {
     });
 
     this.vel = ex.vec(0, -500);
+  }
+
+  override onInitialize(): void {
+    this.graphics.add(Resources.BoltImage.toSprite({ destSize: { width: 16, height: 16 } }));
   }
 
   override onCollisionStart(_self: ex.Collider, other: ex.Collider): void {
--- src/resources.ts
+++ src/resources.ts
@@ -2,4 +2,5 @@ import * as ex from 'excalibur';
 
 export const Resources = {
   PooImage: new ex.ImageSource('./images/angry-poop.png'),
+  BoltImage: new ex.ImageSource('./images/bolt.png'),
 } as const;
